dcccd78a2626d9cbdb59abd1e2acd25a03e1644b
Patrick Evans <ppevans11@gmail.com>
2020-06-05 15:24:24 +1200

    :sparkles: Updated run.sh

--- run.sh
+++ run.sh
@@ -10,4 +10,11 @@ while true;
   sleep 10;
 done
 
+# cd stock-cli
+# while true;
+#   do clear;
+#   ./stock.sh ASP.NZ NPF.NZ IFT.NZ KPG.NZ AIA.NZ MLN.NZ KFL.NZ POT.NZ ARG.NZ SPG.NZ;
+#   sleep 10;
+# done
+
 # EOF #
