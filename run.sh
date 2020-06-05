7261ac9e074834476fcadb923c1128ac9508200c
Patrick Evans <ppevans11@gmail.com>
2020-06-05 15:22:19 +1200

    :sparkles: Updated run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
+# ------ #
+# run.sh #
+# ------ #
 
+cd ticker.sh
+
+while true;
+  do clear;
+  ./ticker.sh ASP.NZ NPF.NZ IFT.NZ KPG.NZ AIA.NZ MLN.NZ KFL.NZ POT.NZ ARG.NZ SPG.NZ;
+  sleep 10;
+done
+
+# EOF #
