83c45fb4f50482b7e325d65b521f3011fa9d0541
Patrick Evans <ppevans11@gmail.com>
2020-06-07 17:41:15 +1200

    :sparkles: Updated run.sh

--- run.sh
+++ run.sh
@@ -6,14 +6,14 @@ cd ticker.sh
 
 while true;
   do clear;
-  ./ticker.sh ASP.NZ NPF.NZ IFT.NZ KPG.NZ AIA.NZ MLN.NZ KFL.NZ POT.NZ ARG.NZ SPG.NZ;
+  ./ticker.sh ASP.NZ NPF.NZ GMT.NZ IFT.NZ KPG.NZ MCY.NZ AIA.NZ MLN.NZ KFL.NZ POT.NZ ARG.NZ SPG.NZ MET.NZ;
   sleep 10;
 done
 
 # cd stock-cli
 # while true;
 #   do clear;
-#   ./stock.sh ASP.NZ NPF.NZ IFT.NZ KPG.NZ AIA.NZ MLN.NZ KFL.NZ POT.NZ ARG.NZ SPG.NZ;
+#   ./stock.sh ASP.NZ NPF.NZ GMT.NZ IFT.NZ KPG.NZ MCY.NZ AIA.NZ MLN.NZ KFL.NZ POT.NZ ARG.NZ SPG.NZ MET.NZ;
 #   sleep 10;
 # done
 
