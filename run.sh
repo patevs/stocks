ce6d2dc105e5e4995d32ac10965c691a1f300df8
Patrick Evans <ppevans11@gmail.com>
2020-06-08 13:49:55 +1200

    :sparkles: Updated run.sh

--- run.sh
+++ run.sh
@@ -6,9 +6,22 @@ cd ticker.sh
 
 while true;
   do clear;
-  ./ticker.sh NZDUSD=X NZDEUR=X GC=F CL=F ^NZ50 ASP.NZ NPF.NZ GMT.NZ IFT.NZ KPG.NZ MCY.NZ KMD.NZ AIA.NZ MLN.NZ KFL.NZ POT.NZ ARG.NZ SPG.NZ MET.NZ;
-  sleep 10;
+  ./ticker.sh \
+    NZDUSD=X NZDEUR=X \
+    GC=F CL=F \
+    ^NZ50 \
+    ASP.NZ NPF.NZ \
+    GMT.NZ IFT.NZ KPG.NZ MCY.NZ \
+    KMD.NZ AIA.NZ MLN.NZ KFL.NZ \
+    POT.NZ ARG.NZ SPG.NZ MET.NZ;
+  sleep 30;
 done
+
+# while true;
+#   do clear;
+#   ./ticker.sh NZDUSD=X NZDEUR=X GC=F CL=F ^NZ50 ASP.NZ NPF.NZ GMT.NZ IFT.NZ KPG.NZ MCY.NZ KMD.NZ AIA.NZ MLN.NZ KFL.NZ POT.NZ ARG.NZ SPG.NZ MET.NZ;
+#   sleep 10;
+# done
 
 # cd stock-cli
 # while true;
