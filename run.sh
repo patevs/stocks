03e1d08c14498cc1975c84e1672acc2c40d46776
Patrick Evans <ppevans11@gmail.com>
2020-06-11 17:14:33 +1200

    :sparkles: Updated run.sh

--- run.sh
+++ run.sh
@@ -2,10 +2,30 @@
 # run.sh #
 # ------ #
 
+# red=$'\e[1;31m'
+# grn=$'\e[1;32m'
+# yel=$'\e[1;33m'
+# blu=$'\e[1;34m'
+# mag=$'\e[1;35m'
+# cyn=$'\e[1;36m'
+
+FG_BLACK=$'\e[1;30m'
+FG_WHITE=$'\e[1;97m'
+
+BG_GREEN=$'\e[1;42m'
+BG_BLUE=$'\e[1;44m'
+# BG_MAGENTA=$'\e[1;45m'
+BG_MAGENTA=$'\e[45m'
+
+end=$'\e[0m'
+
+# echo -e "\e[42m ticker.sh \e[28m"
+
 cd ticker.sh
 
 while true;
   do clear;
+  printf "\n${BG_MAGENTA} ticker.sh ${end} \n\n"
   ./ticker.sh \
     NZDUSD=X NZDEUR=X NZDAUD=X BTCUSD=X \
     CL=F GC=F SI=F \
