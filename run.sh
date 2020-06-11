a0d4d233bec077b748448ff608912f579df5a17d
Patrick Evans <ppevans11@gmail.com>
2020-06-11 16:52:30 +1200

    :sparkles: Updated run.sh

--- run.sh
+++ run.sh
@@ -7,14 +7,14 @@ cd ticker.sh
 while true;
   do clear;
   ./ticker.sh \
-    NZDUSD=X NZDEUR=X \
-    GC=F CL=F \
-    ^NZ50 \
+    NZDUSD=X NZDEUR=X NZDAUD=X BTCUSD=X \
+    CL=F GC=F SI=F \
+    ^NZ50 ^AXJO ^NDX ^DJI \
     ASP.NZ NPF.NZ \
-    GMT.NZ IFT.NZ KPG.NZ MCY.NZ \
-    CEN.NZ MEL.NZ GNE.NZ SPK.NZ RYM.NZ \
-    KMD.NZ AIA.NZ MLN.NZ KFL.NZ \
-    POT.NZ ARG.NZ SPG.NZ MET.NZ;
+    GMT.NZ IFT.NZ KPG.NZ MCY.NZ MET.NZ \
+    POT.NZ CEN.NZ MEL.NZ GNE.NZ SPK.NZ \
+    KMD.NZ AIA.NZ AIR.NZ MLN.NZ KFL.NZ \
+    RYM.NZ ARG.NZ SPG.NZ SKT.NZ SKC.NZ ;
   sleep 30;
 done
 
