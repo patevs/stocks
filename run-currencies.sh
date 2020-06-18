e3ab808d886a474ffce4cbe758e6d82cc0c83a39
Patrick Evans <ppevans11@gmail.com>
2020-06-18 12:35:16 +1200

    :sparkles: Updated run-currencies.sh

--- run-currencies.sh
+++ run-currencies.sh
@@ -1,1 +1,32 @@
+# ----------------- #
+# run-currencies.sh #
+# ----------------- #
+
+# Foreground colors
+FG_BLACK=$'\e[30m'
+
+# Background colors
+BG_YELLOW=$'\e[43m'
+# BG_MAGENTA=$'\e[45m'
+
+# No color
+END=$'\e[0m'
+
+cd ticker.sh
+
+while true;
+  do clear;
+  # printf "\n${BG_MAGENTA}  ticker.sh  ${END} \n\n"
+  printf "${BG_YELLOW}${FG_BLACK} CURRENCIES ${END}\n\n"
+  ./ticker.sh \
+    NZDUSD=X NZDEUR=X NZDAUD=X BTCUSD=X;
+  printf "\n${BG_YELLOW}${FG_BLACK} COMMODITIES ${END}\n\n"
+  ./ticker.sh \
+    CL=F GC=F SI=F;
+  printf "\n${BG_YELLOW}${FG_BLACK} EXCHANGES ${END}\n\n"
+  ./ticker.sh \
+    ^NZ50 ^AXJO ^NDX ^DJI;
+done
+
+# EOF #
 
