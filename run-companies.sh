3d45c543f00ccc8215bfcae0b0551ab7e9b7bfa9
Patrick Evans <ppevans11@gmail.com>
2020-06-18 12:36:05 +1200

    :sparkles: Updated run-companies.sh

--- run-companies.sh
+++ run-companies.sh
@@ -1,1 +1,30 @@
+# ------ #
+# run.sh #
+# ------ #
 
+# Foreground colors
+FG_BLACK=$'\e[30m'
+
+# Background colors
+BG_YELLOW=$'\e[43m'
+
+# No color
+END=$'\e[0m'
+
+cd ticker.sh
+
+while true;
+  do clear;
+  printf "\n${BG_YELLOW}${FG_BLACK} ETFs ${END}\n\n"
+  ./ticker.sh \
+    ASP.NZ NPF.NZ;
+  printf "\n${BG_YELLOW}${FG_BLACK} COMPANIES ${END}\n\n"
+  ./ticker.sh \
+    GMT.NZ IFT.NZ KPG.NZ MCY.NZ MET.NZ \
+    POT.NZ CEN.NZ MEL.NZ GNE.NZ SPK.NZ \
+    KMD.NZ AIA.NZ AIR.NZ MLN.NZ KFL.NZ \
+    RYM.NZ ARG.NZ SPG.NZ SKT.NZ SKC.NZ ;
+  sleep 30;
+done
+
+# EOF #
