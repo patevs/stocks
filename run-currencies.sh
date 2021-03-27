3cb0a0f3eeedafd057a4172f3d89d486e2f4dd41
Patrick Evans <ppevans11@gmail.com>
2021-03-27 15:44:09 +1300

    :heavy_minus_sign: Removed BTCUSD=X from run-currencies.sh

--- run-currencies.sh
+++ run-currencies.sh
@@ -18,7 +18,7 @@ while true;
   # printf "\n${BG_MAGENTA}  ticker.sh  ${END} \n\n"
   printf "${BG_YELLOW}${FG_BLACK} CURRENCIES ${END}\n\n"
   ./ticker.sh \
-    NZDUSD=X NZDEUR=X NZDAUD=X BTCUSD=X;
+    NZDUSD=X NZDEUR=X NZDAUD=X; # BTCUSD=X
   printf "\n${BG_YELLOW}${FG_BLACK} COMMODITIES ${END}\n\n"
   ./ticker.sh \
     CL=F GC=F SI=F;
